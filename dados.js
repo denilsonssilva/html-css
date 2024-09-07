38411323a7d64aa820a113a970b980a51be7dba6
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-07 10:21:11 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados = [
+    {
+        titulo: 'Jumanji',
+        sinopse: 'Jumanji é uma franquia de filmes de aventura e fantasia...',
+        genero: 'Aventura',
+        duracao: '104 minutos',
+        tags: 'tabuleiro jogo'
+    },
+    {
+        titulo: 'Indiana Jones e os Caçadores da Arca Perdida',
+        sinopse: 'Um arqueólogo aventureiro embarca em uma busca pela Arca da Aliança.',
+        genero: 'Aventura',
+        duracao: '115 minutos',
+        tags: 'Arqueologo professor'
+    },
+    {
+        titulo: 'Piratas do Caribe: A Maldição do Pérola Negra',
+        sinopse: 'Um jovem marinheiro se junta a um grupo de piratas em busca de um tesouro.',
+        genero: 'Aventura',
+        duracao: '143 minutos',
+        tags: 'navio Jhonny Deep'
+    },
+    {
+        titulo: 'Moana',
+        sinopse: 'Uma jovem princesa polinésia embarca em uma jornada para salvar seu povo.',
+        genero: 'Aventura',
+        duracao: '107 minutos',
+        tags: 'animação Animação'
+    }
+
+];
+
+
